9408cb4e8665a5f52d5ee4b9ca3cfbbe0d204a58
Pjsanchezh <pjsanchezh@gmail.com>
2021-10-02 18:50:28 -0300

    Added js to the html

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    
+    // Tooltip bootstrap5 
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+
+  console.log("Funcionando los sistemas!");
+});
